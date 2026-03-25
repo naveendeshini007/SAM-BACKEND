dee7926820d8c06cd9653e381557be0f937fb05c
Shubham Nagar <shubham.nagar@yottatechports.com>
2026-03-25 19:02:05 +0530

    Added GET, FILTER, SEARCH APIs for sam_data (#1)
    * Added GET, FILTER, SEARCH APIs for sam_data
    
    * Implemented sam_data APIs with pagination, search, and filtering
    
    * implemented the api's related to get org and search org
    
    * added the migration files
    
    * Added the new Migration file

--- app/api/v1/endpoints/sam_data.py
+++ app/api/v1/endpoints/sam_data.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException, status, Depends
+from app.services.sam_download import SamDownloadService
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.services.sam_data_service import SamDataService
-from app.services.sam_download import SamDownloadService
 from app.core.database import get_db
 from typing import Optional
 from app.schemas.pagination import PaginationParams
@@ -98,3 +98,43 @@ async def download_sam_data(year: int, month: int):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=str(e)
         )
+    
+#GET ORGANIZATIONS
+@router.get("/organizations")
+async def fetch_organizations(
+    pagination: PaginationParams = Depends(),
+    db: AsyncSession = Depends(get_db),
+):
+    return await SamDataService.get_organizations(
+        db=db,
+        page=pagination.page,
+        limit=pagination.limit
+    )
+
+# GET SINGLE ORGANIZATION
+@router.get("/organizations/{record_id}")
+async def fetch_organization(
+    record_id: str,
+    db: AsyncSession = Depends(get_db)
+):
+    return await SamDataService.get_organization_by_id(db, record_id)
+
+#GET FILTER & SEARCH API  
+@router.get("/organizations/search")
+async def search_organizations(
+    pagination: PaginationParams = Depends(),
+    search: Optional[str] = None,
+    state: Optional[str] = None,
+    year: Optional[str] = None,
+    month: Optional[str] = None,
+    db: AsyncSession = Depends(get_db),
+):
+    return await SamDataService.search_organizations(
+        db=db,
+        page=pagination.page,
+        limit=pagination.limit,
+        search=search,
+        state=state,
+        year=year,
+        month=month
+    )
