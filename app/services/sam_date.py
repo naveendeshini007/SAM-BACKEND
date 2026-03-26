dbafa1109e5a0b1372fa72e9be76cff7bafcce9e
Your Name <you@example.com>
2026-03-26 16:06:19 +0530

    sam_date

--- app/services/sam_date.py
+++ app/services/sam_date.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-
+from fastapi import HTTPException, status
 
 class SamDateService:
 
@@ -9,8 +9,14 @@ class SamDateService:
         Returns file_date in format YYYYMMDD
         where DD is first Sunday of the month
         """
-        first_day = datetime(year, month, 1)
-        days_to_sunday = (6 - first_day.weekday()) % 7
-        first_sunday = first_day + timedelta(days=days_to_sunday)
+        try:
+            first_day = datetime(year, month, 1)
+            days_to_sunday = (6 - first_day.weekday()) % 7
+            first_sunday = first_day + timedelta(days=days_to_sunday)
 
-        return first_sunday.strftime("%Y%m%d")
+            return first_sunday.strftime("%Y%m%d")
+        except Exception as exception:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Error calculating first Sunday"
+            )
